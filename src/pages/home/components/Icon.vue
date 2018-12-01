3436aa6e6e1c9bb35f2941c191289c5ca5b08604
ee <ee@163.com>
2018-12-01 15:00:58 +0800

    icon finish

---
+++ src/pages/home/components/Icon.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="(item,index) of page" :key="index">
+        <div class="icon-img">
+          <img class="icon-img-content" :src="item.imgUrl" alt="">
+        </div>
+        <p class="icon-desc">{{item.desc}}</p>
+      </div>
+      </swiper-slide>
+
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcon',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/87/20656da0ac042002.png',
+        desc: '玻璃栈道'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '东莞必游'
+      }, {
+        id: '0005',
+        imgUrl: 'https://img1.qunarzz.com/piao/fusion/1811/1d/c46140e32fffa502.png',
+        desc: '去哪臻选'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+        desc: '亲子游'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+        desc: '玩转长隆'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+        desc: '自然风光'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/48/cb72b99b71974c02.png',
+        desc: '生活休闲'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "~styles/variables";
+  .icons /deep/ .swiper-container {
+    width: 100%;
+    height: 0;
+    padding-bottom: 50%;
+  }
+
+  .icons {
+
+    .icon {
+      position: relative;
+      width: 25%;
+      height: 0;
+      padding-bottom: 25%;
+      float: left;
+    }
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: .44rem;
+      box-sizing: border-box;
+      padding: .1rem;
+      .icon-img-content {
+        display: block;
+        margin: 0 auto;
+        height: 100%;
+      }
+    }
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      height: .44rem;
+      line-height: .44rem;
+      text-align: center;
+      color: $darkTextColor;
+      @include ellipsis;
+    }
+
+  }
+</style>
